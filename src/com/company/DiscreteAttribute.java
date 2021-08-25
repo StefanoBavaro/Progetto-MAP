8f2738033f8a48ae73a55ef0289e4204c18ffc58
jacopodabramo <j.dabramo@studenti.uniba.it>
2021-08-25 10:45:07 +0200

    corretto errore

--- src/com/company/DiscreteAttribute.java
+++ src/com/company/DiscreteAttribute.java
@@ -2,11 +2,12 @@ package com.company;
 
  class DiscreteAttribute extends Attribute {
 
-    private String values[];
+    private String[] values;
 
-    DiscreteAttribute(final String name, final int index, final String values[]) {
+    DiscreteAttribute(final String name, final int index, final String[] values) {
         super(name, index);
         this.values = new String[values.length];
+        this.values = values;
     }
 
     int getNumberOfDistinctValues() {
