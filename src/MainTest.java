6ae2d8fa00a5d6be6f1a06abafa8a4847a3dff51
Stefano Bavaro <hufflepuffc@gmail.com>
2021-08-26 23:36:29 +0200

    Aggiornato main con interazioni utente

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,5 +1,6 @@
 import data.Data;
 import data.EmptySetException;
+import keyboardinput.Keyboard;
 import mining.EmergingPatternException;
 import mining.EmergingPatternMiner;
 import mining.FrequentPatternMiner;
@@ -17,6 +18,21 @@ public class MainTest {
 		
 		System.out.println("Background data");
 		System.out.println(dataBackground);
+
+		float minSup;
+		do{
+			System.out.println("Inserire valore minimo supporto (minSup>0 e minSup<=1):");
+			minSup= Keyboard.readFloat();
+		}while(minSup<=0 || minSup>1);
+
+		float minGr;
+		do{
+			System.out.println("Inserire valore minimo grow rate (minGr>0):");
+			minGr=Keyboard.readFloat();
+		}while(minGr<=0);
+
+
+
 		try{
 			FrequentPatternMiner fpMiner = new FrequentPatternMiner(dataTarget, 0.3f);
 			System.out.println("Frequent patterns");
