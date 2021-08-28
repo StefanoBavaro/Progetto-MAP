72b86f9a50cd6984671c55b29adbe85c750d1862
LorenzoCassano <l.cassano25@studenti.uniba.it>
2021-08-28 15:03:05 +0200

    Creata classe ContinuosAttributeIterator

---
+++ src/data/ContinuousAttributeIterator.java
@@ -0,0 +1,36 @@
+package data;
+
+import java.util.Iterator;
+
+public class ContinuousAttributeIterator implements Iterator<Float> {
+
+	private float min;
+	private float max;
+	private int j = 0;
+	private int numValues;
+	
+	ContinuousAttributeIterator(float min, float max, int numValues){
+		this.min = min;
+		this.max = max;
+		this.numValues = numValues;
+	}
+	
+	@Override
+	public boolean hasNext() {
+		
+		return (j <= numValues);
+	}
+
+
+	public Float next() {
+		j++;
+		return min + ((max-min) / numValues) * (j - 1);
+	}
+
+	
+	public void remove() {
+	
+
+	}
+
+}
